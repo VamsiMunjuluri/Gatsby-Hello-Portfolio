1937fa4a3b46cd4a66147997aadd4bb400c6f98a
Vamsi Munjuluri <nagavamsi694@gmail.com>
2020-05-16 00:27:36 +0530

    enter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 import React from "react"
 
-export default () => <div>Hows it goin? What's up?</div>
+export default () => <div>Hows it goin? What's up guys?</div>
 alert("Welcome to my portofolio website!!");
